6c420a119b3e4fae5a052eb99028f028328ca9c5
jm <desarrollador.senior@ada.co>
2024-08-05 22:27:42 -0500

    ajustes con cliente

---
+++ src/app/pipe/date-time-format-pipe.ts
@@ -0,0 +1,17 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateTimeFormat'
+})
+export class DateTimeFormatPipe implements PipeTransform {
+  transform(value: string): string {
+    const date = new Date(value);
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  }
+}
