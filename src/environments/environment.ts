60593657f4104e77196805c39761262728e9ff23
jm <desarrollador.senior@ada.co>
2024-08-05 23:55:17 -0500

    ajustes con cliente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080/api',
+    apiUrl: 'http://prestadores.recetalia.com:8080/api',
     otherConfig: 'someOtherValue'
 };
   
