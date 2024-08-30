d0089809c3f55bc21bebad41341fdb7f46707369
jmm <desarrollador.senior@ada.co>
2024-08-29 22:21:19 -0500

    ajustes front

--- src/app/pipe/date-time-format-pipe.ts
+++ src/app/pipe/date-time-format-pipe.ts
@@ -11,7 +11,7 @@ export class DateTimeFormatPipe implements PipeTransform {
     const day = ('0' + date.getDate()).slice(-2);
     const hours = ('0' + date.getHours()).slice(-2);
     const minutes = ('0' + date.getMinutes()).slice(-2);
-
-    return `${year}-${month}-${day} ${hours}:${minutes}`;
+    return `${year}-${month}-${day}`;
+    // return `${year}-${month}-${day} ${hours}:${minutes}`;
   }
 }
